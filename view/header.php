87df25db4ba6ad915065ca14e435e6c82857cefb
d00215406 <47351973+d00215406@users.noreply.github.com>
2019-02-13 13:35:44 +0000

    The first commit of my view/header.php

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Bicycle Reviews</title>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Bicycle Reviews</h1></header>
+       
