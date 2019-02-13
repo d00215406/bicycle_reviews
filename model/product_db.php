c7dadfb715e3b1a17facb16050098ddeeae25f4e
d00215406 <47351973+d00215406@users.noreply.github.com>
2019-02-13 13:39:48 +0000

    The first commit of my model/product_db.php

---
+++ model/product_db.php
@@ -0,0 +1,80 @@
+<?php
+function get_bicycles() {
+    global $db;
+    $query = 'SELECT * FROM bicycles
+              ORDER BY productID';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $bicycles = $statement->fetchAll();
+    $statement->closeCursor();
+    return $bicycles;
+}
+
+function get_bicycles_by_category($category_id) {
+    global $db;
+    $query = 'SELECT * FROM bicycles
+              WHERE bicycles.categoryID = :category_id
+              ORDER BY productID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":category_id", $category_id);
+    $statement->execute();
+    $bicycles = $statement->fetchAll();
+    $statement->closeCursor();
+    return $bicycles;
+}
+
+function get_product($product_id) {
+    global $db;
+    $query = 'SELECT * FROM bicycles
+              WHERE productID = :product_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":product_id", $product_id);
+    $statement->execute();
+    $product = $statement->fetch();
+    $statement->closeCursor();
+    return $product;
+}
+
+function delete_product($product_id) {
+    global $db;
+    $query = 'DELETE FROM bicycles
+              WHERE productID = :product_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':product_id', $product_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function add_product($category_id, $code, $name, $price) {
+    global $db;
+    $query = 'INSERT INTO bicycles
+                 (categoryID, productCode, productName, listPrice)
+              VALUES
+                 (:category_id, :code, :name, :price)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->bindValue(':code', $code);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':price', $price);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function update_product($product_id, $category_id, $code, $name, $price) {
+    global $db;
+    $query = 'UPDATE bicycles
+              SET categoryID = :category_id,
+                  productCode = :code,
+                  productName = :name,
+                  listPrice = :price
+               WHERE productID = :product_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->bindValue(':code', $code);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':product_id', $product_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+?>
