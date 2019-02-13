334bdedaef8b42826746b98dc9db408611cf58dc
d00215406 <47351973+d00215406@users.noreply.github.com>
2019-02-13 13:34:45 +0000

    The first commit of my index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'view/header.php'; ?>
+<link href="main.css" rel="stylesheet" type="text/css"/>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Bicycle Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Bicycle Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
